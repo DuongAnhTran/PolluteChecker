46afa75ed227f2003807e0456a9040295238200d
Duong Anh Tran <tranduonganh2004@gmail.com>
2025-09-25 15:15:04 +1000

    Initial Commit

---
+++ PolluteChecker/PolluteCheckerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PolluteCheckerApp.swift
+//  PolluteChecker
+//
+//  Created by Dương Anh Trần on 25/9/2025.
+//
+
+import SwiftUI
+
+@main
+struct PolluteCheckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
